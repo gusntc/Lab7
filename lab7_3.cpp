6dee83bd6d9980668e71228e233d42aae0505318
gusntc <Natacha_rungbanpant@cmu.ac.th>
2021-11-30 18:41:32 +0700

    Lab7_3Edit

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,37 @@
 
 using namespace std;
 
+int adiff(int A,int B){
+  if(A >= 360 or B >= 360){
+    A = A%360;
+    B = B%360;
+  }
+  int C = A-B;
+  if(C%360 == 0){
+    return 0;
+  }else{
+    if(C > 0){
+      if(C >= 180){
+        return 360-C;
+      }
+    return C;
+    }else{
+      if(C < -180){
+        return C+360;
+      }
+    return -C;
+    }
+  }
+}
+ 
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
   cout << adiff(60,244);
 }
